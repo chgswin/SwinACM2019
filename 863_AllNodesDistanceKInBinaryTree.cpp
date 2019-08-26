b7d5fde2155ee1398b4237df70128dbd16da150d
Chuong Ho <41665273+chgswin@users.noreply.github.com>
2019-08-26 20:36:04 +1000

    Update 863_AllNodesDistanceKInBinaryTree.cpp

--- 863_AllNodesDistanceKInBinaryTree.cpp
+++ 863_AllNodesDistanceKInBinaryTree.cpp
@@ -13,10 +13,13 @@ private:
     unordered_set<TreeNode*> visited;
     unordered_map<TreeNode*, int> distance;
     
+    //using DFS to find the parent of each node
+    //root parent ignored to reduce size of code but still working correctly
     void Find_Parent_DFS(TreeNode* node)
     {
         if (node)
         {
+            //if (node->left) is the short way to check if (node->left != NULL)
             if (node->left) parent_map[node->left] = node;
             if (node->right) parent_map[node->right] = node;
             Find_Parent_DFS(node->left);
@@ -43,6 +46,15 @@ public:
                 visited.insert(curNode);
                 if (distance[curNode] == K) res.push_back(curNode->val);
                 else if (distance[curNode] > K) break;
+                //uset means unordered_set
+                //umap means unordered_map
+                //
+                //@Hung @Linh: I just found the way we shortened the code for uset.find() != uset.end()
+                //
+                //so instead of writing uset.find(element) != uset.end() (in this example we check whether the element is in the uset
+                //we just need to write if (uset.count(element)), if element is in, uset.count(element) will surely > 0 which returns true
+                //later on
+                //
                 if (curNode->left && !visited.count(curNode->left))
                 {
                     distance[curNode->left] = distance[curNode] + 1;
